7ab082c88533a020323c76e11cc55282da9e219f
Liam <112492930+ElSammen@users.noreply.github.com>
2023-10-04 16:38:01 +0100

    modified card component for passing value/suit as props for later feature
    TODO: outs quiz

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,36 +1,32 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Container from 'react-bootstrap/Container';
-import '../board.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import PHand from '../Player/PHand';
-import { CloseButton } from 'react-bootstrap';
-
-
+import React, { useState, useEffect, useRef } from "react";
+import Container from "react-bootstrap/Container";
+import "../board.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PHand from "../Player/PHand";
+import { CloseButton } from "react-bootstrap";
 
 function Board() {
-
-  const [cardInfo, setCardInfo] = useState("")
+  const [cardInfo, setCardInfo] = useState("");
   const [boardData, setBoardData] = useState([
     { card: 1, cardInfo: "" },
     { card: 2, cardInfo: "" },
     { card: 3, cardInfo: "" },
     { card: 4, cardInfo: "" },
-    { card: 5, cardInfo: "" }
-  ])
+    { card: 5, cardInfo: "" },
+  ]);
 
-  const [turn, setTurn] = useState(false)
-  const [river, setRiver] = useState(false)
+  const [turn, setTurn] = useState(false);
+  const [river, setRiver] = useState(false);
 
   const getCardInfo = (data) => {
-    console.log("do it work", data)
+    console.log("do it work", data);
     setCardInfo(data);
     if (data) {
-      setUserCards(true)
+      setUserCards(true);
     }
+  };
 
-  }
-
-  const [userCards, setUserCards] = useState(false)
+  const [userCards, setUserCards] = useState(false);
 
   const card1Ref = useRef(null);
   const card2Ref = useRef(null);
@@ -38,109 +34,160 @@ function Board() {
   const card4Ref = useRef(null);
   const card5Ref = useRef(null);
 
-  const [pot, setPot] = useState("")
-  const [bet, setBet] = useState("")
+  const [pot, setPot] = useState("");
+  const [bet, setBet] = useState("");
 
   const potOddBoxes = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       if (e.target.id === "potbox") {
-        setPot(e.target.value)
-      }
-      else if (e.target.id === "betbox") {
-        setBet(e.target.value)
+        setPot(e.target.value);
+      } else if (e.target.id === "betbox") {
+        setBet(e.target.value);
       }
     }
-  }
+  };
 
   const potOdds = () => {
     if (pot && bet) {
-      let potOdds = (bet / (pot + bet)) * 10000
-      return potOdds.toFixed(0)
-            
+      let potOdds = (bet / (pot + bet)) * 10000;
+      return potOdds.toFixed(0);
     }
-  }
+  };
 
-  const [selectedOption, setSelectedOption] = useState("potOdds")
+  const [selectedOption, setSelectedOption] = useState("potOdds");
 
   const handleOptionChange = (e) => {
     if (e.target.id === "potOdds") {
-      setSelectedOption("potOdds")
+      setSelectedOption("potOdds");
+    } else if (e.target.id === "cardView") {
+      setSelectedOption("cardView");
     }
-    else if (e.target.id === "cardView") {
-      setSelectedOption("cardView")
-    }
-  }
+  };
 
   return (
     <Container fluid className="board">
       <CloseButton className="clearButton" />
-      <div className='radioButtons'>
+      <div className="radioButtons">
         <div className="radioTitle">Select Option</div>
-        <div className='potOddsDiv'>
-          <input id="potOdds" name="radioOption" type="radio" defaultChecked={true} onChange={handleOptionChange}></input>
-        <label htmlFor="potOdds">Pot Odds</label>
+        <div className="potOddsDiv">
+          <input
+            id="potOdds"
+            name="radioOption"
+            type="radio"
+            defaultChecked={true}
+            onChange={handleOptionChange}
+          ></input>
+          <label htmlFor="potOdds">Pot Odds</label>
         </div>
-        <div className='cardViewDiv'>
-        <input id="cardView" name="radioOption" type="radio"  onChange={handleOptionChange}></input>
-        <label htmlFor="cardView">Card View</label>
+        <div className="cardViewDiv">
+          <input
+            id="cardView"
+            name="radioOption"
+            type="radio"
+            onChange={handleOptionChange}
+          ></input>
+          <label htmlFor="cardView">Card View</label>
         </div>
       </div>
-     {selectedOption === "potOdds" ? <>
-     <div className="numberInput">
-        <div><p >The pot will be...</p>
-        <input type="text" className="numberInput" placeholder="type here"  onKeyDown={potOddBoxes} id="potbox"/></div>
-        <div> <p>You have to call...</p>
-        <input type="text" className="numberInput" placeholder="type here"  onKeyDown={potOddBoxes} id="betbox"/>
-        </div>
-        { potOdds ? <div><p>So you need...</p>
-        <div className="potOddsResult">{potOdds()}%</div>
-        </div> : null}
-      </div>
-      </> : null}
-      {selectedOption === "cardView" ?
-      <>
-       <Container fluid className="boardCards px-0 w-auto">
-        <PHand
-          key="boardcard1"
-          getCardInfo={() => getCardInfo(card1Ref)}
-          ref={card1Ref}
-        />
-        <PHand
-          key="boardcard2"
-          getCardInfo={() => getCardInfo(card2Ref)}
-          ref={card2Ref}
-        />
-        <PHand
-          key="boardcard3"
-          getCardInfo={() => getCardInfo(card3Ref)}
-          ref={card3Ref}
-        />
-        {turn? <PHand
-          key="boardcard4"
-          getCardInfo={() => getCardInfo(card4Ref)}
-          ref={card4Ref}
-        /> : 
-        <div className='boardCardUnfilled'>
-          <div className="turnButton" onClick={()=>setTurn(true)}>
-            <p>+</p>
+      {selectedOption === "potOdds" ? (
+        <>
+          <div className="numberInput">
+            <div>
+              <p>Pot is...</p>
+              <input
+                type="text"
+                className="numberInput"
+                placeholder="....."
+                onKeyDown={potOddBoxes}
+                id="potbox"
+              />
             </div>
-        </div>
-        }
-         {river?<PHand
-          key="boardcard5"
-          getCardInfo={() => getCardInfo(card5Ref)}
-          ref={card5Ref}
-        />:null}
-      </Container>
-      <Container fluid className="playerHand px-0 w-auto">
-        <PHand key="usercard1" getCardInfo={getCardInfo} id="usercard1" className="pcard1" />
-        <PHand key="usercard2" getCardInfo={getCardInfo} id="usercard2" className="pcard2" />
-      </Container>
-      </>
-      : null
+            <div>
+              {" "}
+              <p>You have to call...</p>
+              <input
+                type="text"
+                className="numberInput"
+                placeholder="....."
+                onKeyDown={potOddBoxes}
+                id="betbox"
+              />
+            </div>
+            {potOdds ? (
+              <div>
+                <p>So you need...</p>
+                <div className="potOddsResult">{potOdds()}%</div>
+              </div>
+            ) : null}
+          </div>
+        </>
+      ) : null}
+      {selectedOption === "cardView" ? (
+        <>
+          <Container fluid className="boardCards px-0 w-auto">
+            <PHand
+              key="boardcard1"
+              getCardInfo={() => getCardInfo(card1Ref)}
+              ref={card1Ref}
+            />
+            <PHand
+              key="boardcard2"
+              getCardInfo={() => getCardInfo(card2Ref)}
+              ref={card2Ref}
+            />
+            <PHand
+              key="boardcard3"
+              getCardInfo={() => getCardInfo(card3Ref)}
+              ref={card3Ref}
+            />
+            {turn ? (
+              <>
+                <>
+                  <PHand
+                    key="boardcard4"
+                    getCardInfo={() => getCardInfo(card4Ref)}
+                    ref={card4Ref}
+                  />
+                </>
+                {river ? null :<>
+                  <div className="boardCardUnfilled">
+                    <div className="turnButton" onClick={() => setRiver(true)}>
+                      <p>+</p>
+                    </div>
+                  </div>
+                </>}
+              </>
+            ) : (
+              <div className="boardCardUnfilled">
+                <div className="turnButton" onClick={() => setTurn(true)}>
+                  <p>+</p>
+                </div>
+              </div>
+            )}
+            {river ? (
+              <PHand
+                key="boardcard5"
+                getCardInfo={() => getCardInfo(card5Ref)}
+                ref={card5Ref}
+              />
+            ) : null}
+          </Container>
+          <Container fluid className="playerHand px-0 w-auto">
+            <PHand
+              key="usercard1"
+              getCardInfo={getCardInfo}
+              id="usercard1"
+            />
+            <PHand
+              key="usercard2"
+              getCardInfo={getCardInfo}
+              id="usercard2"
+              className="pcard2"
+            />
+          </Container>
+        </>
+      ) : null}
+    </Container>
+  );
 }
-    </Container> 
-   
-  )
-}
-export default Board
+export default Board;
--- src/Components/Player/PHand.js
+++ src/Components/Player/PHand.js
@@ -1,115 +1,222 @@
-import React, { useEffect, useState } from 'react'
-import { Card, Container } from 'react-bootstrap';
-import '../board.scss'
-import clubsImg from '../images/Cards/clubs.png'
-import diamondsImg from '../images/Cards/diamond.png'
-import heartsImg from '../images/Cards/heart.png'
-import spadesImg from '../images/Cards/spades.png'
-import Default from '../images/Cards/blank.png'
-
-
+import React, { useEffect, useState } from "react";
+import { Card, Container } from "react-bootstrap";
+import "../board.scss";
+import clubsImg from "../images/Cards/clubs.png";
+import diamondsImg from "../images/Cards/diamond.png";
+import heartsImg from "../images/Cards/heart.png";
+import spadesImg from "../images/Cards/spades.png";
+import Default from "../images/Cards/blank.png";
 
 function PHand(props) {
   const [display, setDisplay] = useState("notdisplayed");
   const [selected, setSelected] = useState("notselected");
-  const showButton = e => {
+
+  const [lockedState, setLockedState] = useState("false");
+
+  if (props.locked === "true") {
+    setLockedState("true");
+  }
+
+  const showButton = (e) => {
     e.preventDefault();
-    
-    
-    setDisplay("displayed");
+    if (lockedState === "false") {
+      setDisplay("displayed");
+    }
   };
 
-  const hideButton = e => {
+  const hideButton = (e) => {
     e.preventDefault();
-    if (selected === "false" ){
-      
-    }
     setDisplay("notdisplayed");
   };
 
-const [CardType, setCardType] = useState("Card")
-const [CardImg, setCardImg] = useState(Default)
-const [cardValue, setCardValue] = useState("")
-const [cardState, setCardState] = useState({
-  CardType,
-  cardValue
-})
+  const [CardType, setCardType] = useState("Card");
+  const [CardImg, setCardImg] = useState(Default);
+  const [cardValue, setCardValue] = useState("");
+  const [cardState, setCardState] = useState({
+    CardType,
+    cardValue,
+  });
 
-const handleEntry = (e) => {
-  if (e.key === 'Enter') {
-    if (e.target.value === "11" || e.target.value === "J" || e.target.value === "j" || e.target.value === "Jack" || e.target.value === "jack")
-      setCardValue("J")
-    else if (e.target.value === "12" || e.target.value === "Q" || e.target.value === "q" || e.target.value === "Queen" || e.target.value === "queen")
-      setCardValue("Q")
-    else if (e.target.value === "13" || e.target.value === "K" || e.target.value === "k" || e.target.value === "King" || e.target.value === "king")
-      setCardValue("K")
-    else if (e.target.value === "14" || e.target.value === "A" || e.target.value === "1" || e.target.value === "a" || e.target.value === "Ace" || e.target.value === "ace")
-      setCardValue("A")
-    else if (e.target.value > 1 && e.target.value < 11)
-      setCardValue(e.target.value)
-    else
-      console.log("Invalid card value entered")
-  }
-}
+  // const cardModal = () => {
+  //   return <Card></Card>;
+  // };
 
-useEffect(() => {
-  if (cardValue && CardType) {
-    setSelected("selected")
-    setCardState(cardValue + CardType)
-    console.log(selected, cardState)
-  }
-}, [cardValue, CardType])
+  const handleEntry = (e) => {
+    if (e.key === "Enter") {
+      if (
+        e.target.value === "11" ||
+        e.target.value === "J" ||
+        e.target.value === "j" ||
+        e.target.value === "Jack" ||
+        e.target.value === "jack"
+      )
+        setCardValue("J");
+      else if (
+        e.target.value === "12" ||
+        e.target.value === "Q" ||
+        e.target.value === "q" ||
+        e.target.value === "Queen" ||
+        e.target.value === "queen"
+      )
+        setCardValue("Q");
+      else if (
+        e.target.value === "13" ||
+        e.target.value === "K" ||
+        e.target.value === "k" ||
+        e.target.value === "King" ||
+        e.target.value === "king"
+      )
+        setCardValue("K");
+      else if (
+        e.target.value === "14" ||
+        e.target.value === "A" ||
+        e.target.value === "1" ||
+        e.target.value === "a" ||
+        e.target.value === "Ace" ||
+        e.target.value === "ace"
+      )
+        setCardValue("A");
+      else if (e.target.value > 1 && e.target.value < 11)
+        setCardValue(e.target.value);
+      else console.log("Invalid card value entered");
+    }
+  };
 
+  useEffect(() => {
+    if (cardValue && CardType) {
+      setSelected("selected");
+      setCardState(cardValue + CardType);
+      console.log(selected, cardState);
+    }
+  }, [cardValue, CardType]);
 
-const handleClick = (e) => {
+  useEffect(() => {
+    switch (props.suit) {
+      case "club":
+        setCardType("Club");
+        setCardImg(clubsImg);
+        setSelected("selected");
+        break;
+      case "diamond":
+        setCardType("Diamond");
+        setCardImg(diamondsImg);
+        setSelected("selected");
+        break;
+      case "spade":
+        setCardType("Spade");
+        setCardImg(spadesImg);
+        setSelected("selected");
+        break;
+      case "heart":
+        setCardType("Heart");
+        setCardImg(heartsImg);
+        setSelected("selected");
+        break;
+      default:
+        return;
+    }
+  }, [props.suit]);
+
+  useEffect(() => {
+    switch (props.value) {
+      case "11":
+      case "J":
+      case "j":
+      case "Jack":
+      case "jack":
+        setCardValue("J");
+        break;
+      case "12":
+      case "Q":
+      case "q":
+      case "Queen":
+      case "queen":
+        setCardValue("Q");
+        break;
+      case "13":
+      case "K":
+      case "k":
+      case "King":
+      case "king":
+        setCardValue("K");
+        break;
+      case "14":
+      case "A":
+      case "a":
+      case "1":
+      case "Ace":
+        setCardValue("A");
+        break;
+      default:
+        setCardValue(props.value);
+        break;
+    }
+  }, [props.value]);
+
+  const handleClick = (e) => {
     const button = e.target;
     switch (button.alt) {
       case "club":
-        setCardType("Club")
-        setCardImg(clubsImg)
-        setSelected("selected")
+        setCardType("Club");
+        setCardImg(clubsImg);
+        setSelected("selected");
         break;
       case "diamond":
-        setCardType("Diamond")
-        setCardImg(diamondsImg)
-        setSelected("selected")
+        setCardType("Diamond");
+        setCardImg(diamondsImg);
+        setSelected("selected");
         break;
       case "spade":
-        setCardType("Spade")
-        setCardImg(spadesImg)
-        setSelected("selected")
+        setCardType("Spade");
+        setCardImg(spadesImg);
+        setSelected("selected");
         break;
       case "heart":
-        setCardType("Heart")
-        setCardImg(heartsImg)
-        setSelected("selected")
+        setCardType("Heart");
+        setCardImg(heartsImg);
+        setSelected("selected");
         break;
       default:
-        return
+        return;
     }
-  }
+  };
 
   return (
-    <Card className={`pcard ${selected ? "selected" : ""}` }
-      onMouseEnter={e => showButton(e)}
-      onMouseLeave={e => hideButton(e)}
-      id={cardValue}>
-      <Card.Img className="cardBg" src={CardImg} alt="Card image"/>
+    <Card
+      className={`pcard ${selected ? "selected" : ""}`}
+      onMouseEnter={(e) => showButton(e)}
+      onMouseLeave={(e) => hideButton(e)}
+      id={cardValue}
+    >
+      <Card.Img className="cardBg" src={CardImg} alt="Card image" />
       <Card.ImgOverlay>
-        <Card.Title>{cardValue} {CardType}</Card.Title>
+        <Card.Title>
+          {cardValue} {CardType}
+        </Card.Title>
         <Card.Text className="mt-4">
           <Container className="buttonBox">
-            <button className={display} onClick={handleClick}><img className="Suit" src={clubsImg} alt="club"/></button>
-            <button className={display} onClick={handleClick}><img className='Suit' src={diamondsImg} alt="diamond"/></button>
-            <button className={display} onClick={handleClick}><img className='Suit' src={spadesImg} alt="spade"/></button>
-            <button className={display} onClick={handleClick}><img className='Suit' src={heartsImg} alt="heart"/></button>
+            <button className={display} onClick={handleClick}>
+              <img className="Suit" src={clubsImg} alt="club" />
+            </button>
+            <button className={display} onClick={handleClick}>
+              <img className="Suit" src={diamondsImg} alt="diamond" />
+            </button>
+            <button className={display} onClick={handleClick}>
+              <img className="Suit" src={spadesImg} alt="spade" />
+            </button>
+            <button className={display} onClick={handleClick}>
+              <img className="Suit" src={heartsImg} alt="heart" />
+            </button>
           </Container>
-          <input placeholder='#?' onKeyDown={handleEntry} className="inputBox"></input>
+          <input
+            placeholder="#?"
+            onKeyDown={handleEntry}
+            className="inputBox"
+          ></input>
         </Card.Text>
       </Card.ImgOverlay>
     </Card>
   );
 }
 
-
-export default PHand
+export default PHand;
